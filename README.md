22b0b3f73f0d162cfbdde49cc622e6174c4570b6
David Boland <dboland77@gmail.com>
2020-10-15 22:10:13 +0100

    API calls working and key stored as env

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Weather Application
 
+## Prototype
 This contains the prototype for a weather application.
 
 The prototype folder contains two files:
@@ -10,3 +11,20 @@ The prototype folder contains two files:
 These contain the HTML and CSS for a mock-up of the design below:
 
 <img src ="./Front_End_Design.png">
+
+
+## API Calls
+
+We need two API calls
+1. For the current weather to the /current endpoint
+2. For the forecast to the /forecast endpoint
+
+I have use two separate functions to test the API calls
+each take the city id and api key as parameters.
+
+To hide the API key from being seen I am using REACT_APP environment variables for both (in the .env root file)
+
+To ensure that these are populated when I call the API endpoints I hook them with useEffect which runs after these variables have populated (and rendering is complete)
+
+Functional test
+Both API calls are returning valid data.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const LONDON = process.env.REACT_APP_LONDON;
+
+
+// City id rather than name is recommended so I am using this endpoint
+
+function getForecast( id, apiKey) {
+  const fivedayurl = "https://api.openweathermap.org/data/2.5/forecast?"
+
+  fetch(fivedayurl +'id=' + id+ '&appid=' + apiKey)  
+  .then(response => response.json()) 
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+
+function App() {  
+    useEffect(() => {      
+      getCurrentWeather(LONDON, API_KEY); 
+      getForecast(LONDON, API_KEY); 
+}, []);  
+
+function getCurrentWeather( id, apiKey) {
+  const currenturl = "https://api.openweathermap.org/data/2.5/weather?"
+
+  fetch(currenturl +'id=' + id+ '&appid=' + apiKey)  
+  .then(response=>response.json()) 
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+return(
+  <h1> Checking API call (console log)</h1>
+)
 }
 
 export default App;
